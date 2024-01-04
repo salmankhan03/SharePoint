7232442bfb15f54d7dd920d8c86e0169a4e5cb50
mitesh000790 <mitesh000790@gmail.com>
2024-01-04 18:37:04 +0530

    Remove Default rotation UI set in map

--- src/containers/Map/index.js
+++ src/containers/Map/index.js
@@ -43,6 +43,7 @@ const Map = () => {
             fullscreenControl: false,
             zoomControl: false,
             streetViewControl: false,
+            disableDefaultUI: true,
         },
         onLoad,
         onUnmount,
